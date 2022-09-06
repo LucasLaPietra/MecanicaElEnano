54137a91198745ccb2369889d607e9cceab67a1b
Lucas La Pietra <lelapietra@gmail.com>
2022-09-06 19:51:24 -0300

    feat: added automapper

--- BackendMecanicaElEnano/BackendMecanicaElEnano/Controllers/VehiculoController.cs
+++ BackendMecanicaElEnano/BackendMecanicaElEnano/Controllers/VehiculoController.cs
@@ -1,6 +1,19 @@
-﻿namespace BackendMecanicaElEnano.Controllers
+﻿using BackendMecanicaElEnano.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendMecanicaElEnano.Controllers
 {
-    public class VehiculoController
+    [ApiController]
+    [Route("[vehiculos]")]
+    public class VehiculoController : ControllerBase
     {
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<VehiculoDto> Create(CreateVehiculoDto vechiculo)
+        {
+
+            return CreatedAtAction("","");
+        }
     }
 }
