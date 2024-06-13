8048a0d8629fe23b52f443e89be09df24f1bf208
Lucas La Pietra <lelapietra@gmail.com>
2024-06-13 16:29:32 -0300

    fix: fixed creating vehiculo with empty fields

--- BackendMecanicaElEnano/BackendMecanicaElEnano/Dto/Vehiculo/CreateVehiculoDto.cs
+++ BackendMecanicaElEnano/BackendMecanicaElEnano/Dto/Vehiculo/CreateVehiculoDto.cs
@@ -6,7 +6,7 @@
 
         public string Modelo { get; set; } = null!;
 
-        public string NumeroChasis { get; set; } = null!;
+        public string NumeroChasis { get; set; } = string.Empty;
 
         public string Cliente { get; set; } = null!;
 
@@ -14,8 +14,8 @@
 
         public string Direccion { get; set; } = null!;
 
-        public string Mail { get; set; } = null!;
+        public string Mail { get; set; } = string.Empty;
 
-        public string Cuit { get; set; } = null!;
+        public string Cuit { get; set; } = string.Empty;
     }
 }
